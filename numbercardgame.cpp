05e3a9dbdbfad3e28dd0d1bc975f0f7c7b2d8b0f
hy01n <17hyoinjeong@gm.gist.ac.kr>
2021-05-05 00:01:00 +0900

    05.04

---
+++ numbercardgame.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int main(){
+    int n,m;
+    scanf("%d %d",&n,&m);
+    int arr[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            scanf(" %d",&arr[i][j]);
+        }
+        sort(arr[i],arr[i]+m);
+    }
+    int result=0;
+    for(int i=0;i<n;i++){
+        result = max(result,arr[i][0]);
+    }
+    printf("%d",result);
+    return 0;
+}
