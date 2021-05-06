6b7c70627f22de261d7870820544720256260344
hy01n <17hyoinjeong@gm.gist.ac.kr>
2021-05-07 00:19:48 +0900

    revisited

--- time.cpp
+++ time.cpp
@@ -1,20 +1,31 @@
+//그냥 짜고 될거라 생각말고 예시 넣어보면서 생각
 #include <cstdio>
 
 int main(){
     int n;
     scanf("%d",&n);
     int cnt=0;
+    // for(int i=0;i<=n;i++){
+    //     if(i%10==3||i/10==3) continue;
+    //     for(int j=0;j<60;j++){
+    //         if(j%10==3||j/10==3)  continue;
+    //         for(int k=0;k<60;k++){
+    //             if(k%10==3||k/10==3) continue;
+    //             cnt++;
+    //         }
+    //     }
+    // }
     for(int i=0;i<=n;i++){
-        if(i%10==3||i/10==3) continue;
+        
         for(int j=0;j<60;j++){
-            if(j%10==3||j/10==3)  continue;
+            
             for(int k=0;k<60;k++){
-                if(k%10==3||k/10==3) continue;
-                cnt++;
+                if(k%10==3||k/10==3||j%10==3||j/10==3||i%10==3||i/10==3)cnt++;
+                
             }
         }
     }
-    int k = (n+1)*3600-cnt;
-    printf("%d",k);
+    
+    printf("%d",cnt);
     return 0;
 }
