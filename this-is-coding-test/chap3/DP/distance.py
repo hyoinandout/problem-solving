c21d7bde01c31cd62d835a8e0a20bbd9ee4d668e
hy01n <hobbyhyoin@naver.com>
2021-10-06 14:24:54 +0900

    Fix

--- this-is-coding-test/chap3/DP/distance.py
+++ this-is-coding-test/chap3/DP/distance.py
@@ -4,19 +4,21 @@ b = input()
 # dist = 0
 # cnt = abs(len(b) - len(a))
 # dist += cnt
+n = len(a)
+m = len(b)
 
-dp = [[0] * len(b) for _ in range(len(a))]
-for i in range(len(a)):
+dp = [[0] * (m+1) for _ in range(n+1)]
+for i in range(n+1):
     dp[i][0] = i
-for i in range(len(b)):
+for i in range(m+1):
     dp[0][i] = i
 
-for i in range(1,len(a)):
-    for j in range(1,len(b)):
+for i in range(1,n+1):
+    for j in range(1,m+1):
         if a[i] != b[j]:
             dp[i][j] = 1 + min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
         else:
             dp[i][j] = dp[i-1][j-1]
-print(dp[len(a)-1][len(b)-1])
+print(dp[n][m])
 
 
