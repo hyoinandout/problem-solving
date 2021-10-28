f9b2b9a37f5f1c69e7a57dcdc70c08674e686913
hy01n <hobbyhyoin@naver.com>
2021-10-28 23:32:54 +0900

    practice midterm

---
+++ quicksort_improved.py
@@ -0,0 +1,17 @@
+def partition(array,low,high):
+    i = low-1
+    pivot = array[high]
+    for j in range(low,high):
+        if array[j] <= pivot:
+            i += 1
+            array[i],array[j] = array[j],array[i]
+    array[i+1],array[high] = array[high],array[i+1]
+    return i+1
+        
+def quickSort(array,low,high):
+    if len(array)==1:
+        return array
+    if low < high:
+        pi = partition(array,low,high)
+        quickSort(array,low,pi-1)
+        quickSort(array,pi+1,high)
