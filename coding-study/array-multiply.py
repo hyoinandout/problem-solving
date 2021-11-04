89d45c53931379fa5ecc6d940606b6961e819666
hy01n <hobbyhyoin@naver.com>
2021-11-05 00:31:12 +0900

    Fix

--- coding-study/array-multiply.py
+++ coding-study/array-multiply.py
@@ -1,27 +1,33 @@
-def array_multiply(num, array):
-    if num == 1:
-        return array
-    x = array_multiply(num//2, array)
-    y = array_multiply(num-(num//2), array)
+import copy
 
-    for i in range(len(y)):
-        for j in range(len(y)):
+def array_multiply(num):
+    if num == 1:
+        return arr
+    if num % 2 == 0:
+        arr1 = copy.deepcopy(array_multiply(num//2))
+        arr2 = copy.deepcopy(arr1)
+    else:
+        arr1 = copy.deepcopy(array_multiply(num-1))
+        arr2 = copy.deepcopy(arr)
+    
+    arrResult = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
             partialResult = 0
-            for k in range(len(y)):
-                partialResult += (x[i][k] * y[k][j])
-            array[i][j] = partialResult % 1000
-    return array
+            for k in range(n):
+                partialResult += (arr1[i][k] * arr2[k][j])
+            arrResult[i][j] = partialResult % 1000    
+    return arrResult
 
 n,b = map(int,input().split())
 arr = []
 for _ in range(n):
     arr.append(list(map(int,input().split())))
 
-result = array_multiply(2,arr)
+result = array_multiply(b)
 
 for i in range(n):
     for j in range(n):
         print(result[i][j], end= ' ')
     print()
-
 
