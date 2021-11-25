29820a723fabe1ff3bca312bf5317f28a7fbc2da
hy01n <hobbyhyoin@naver.com>
2021-11-25 11:09:05 +0900

    Solved

--- this-is-coding-test/chap3/samsung/teenager-shark.py
+++ this-is-coding-test/chap3/samsung/teenager-shark.py
@@ -1,3 +1,7 @@
+# 백트래킹 구현
+# 이 때 재귀함수 인자가 local 하게 적용되는지 아니면 global하게 적용되는지 알아서 정리할 것!
+# https://andamiro25.tistory.com/96 : 가변적 개체/ 불가변적 개체 
+import copy
 directionY = [-1,-1,0,1,1,1,0,-1]
 directionX = [0,-1,-1,-1,0,1,1,1]
 
@@ -28,14 +32,7 @@ def checkShark(sharkPos,direction):
 
 def dfs(y,x,space,shark,phase):
     answer = 0
-    print("------------------------------")
-    print(phase)
-    print(y,x)
-    for i in range(4):
-        for j in range(4):
-            print(space[i][j],end=' ')
-        print()
-    shark[0] += space[y][x][0]
+    shark[0] = space[y][x][0]
     shark[1] = space[y][x][1]
     space[y][x] = [0,0]
     sharkPos = [y,x]
@@ -49,15 +46,32 @@ def dfs(y,x,space,shark,phase):
                     break
             if flag:
                 break
-    
+    # print("------------------------------")
+    # print(phase)
+    # for i in range(4):
+    #     for j in range(4):
+    #         print(space[i][j],end=' ')
+    #     print()
+    # print("------------------------------")
+    ny = y
+    nx = x
     if checkShark(sharkPos,shark[1]-1):
-        while 0<= y <4 and 0<= x <4:
-            y += directionY[shark[1]-1]
-            x += directionX[shark[1]-1]
-            answer = max(answer,dfs(y,x,space,shark,phase+1))
-    finalAnswer = 0
-    finalAnswer += answer
-    return finalAnswer
+        while True:
+            ny += directionY[shark[1]-1]
+            nx += directionX[shark[1]-1]
+            #print(ny,nx,shark[1],phase)
+            
+            if 0<= ny and ny <4 and 0<= nx and nx <4:
+                if  space[ny][nx][0] == 0:
+                    continue
+                # 펀치라인
+                sharkCp = copy.deepcopy(shark)
+                print(sharkCp)
+                arr = copy.deepcopy(space)
+                answer = max(answer,dfs(ny,nx,arr,sharkCp,phase+1))
+            else:
+                break
+    return shark[0] + answer
 
 
 space = [[] for _ in range(4)]
@@ -67,7 +81,7 @@ for i in range(4):
         a = row[2*j]
         b = row[2*j + 1]
         space[i].append([a,b])
-print(space)
+#print(space)
 shark = [0,0]
 print(dfs(0,0,space,shark,0))
 
