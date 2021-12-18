01e725b7a28f07708bc8bf765529d5c4d4ace2c1
hy01n <hobbyhyoin@naver.com>
2021-12-18 22:51:33 +0900

    Update

--- leetcode/hw2/p2_bitcoin.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        answer = 0
-        for i in range(len(prices)-1):
-            if prices[i] < prices[i+1]:
-                answer += prices[i+1]-prices[i]
-        return answer
